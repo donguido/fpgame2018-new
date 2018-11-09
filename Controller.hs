cb2771c30e3e56d0fd62d70399306f5c2b2ed575
guido van strien <g.a.l.vanstrien@students.uu.nl>
2018-11-09 16:13:39 +0100

    collisons toegevoegd

--- Controller.hs
+++ Controller.hs
@@ -15,14 +15,15 @@ import System.Random
 
 changeAsteroid :: Asteroid -> Asteroid
 changeAsteroid Asteroid {asteroidX = x , asteroidY = y, asteroidXVector = vx, asteroidYVector = vy} = Asteroid { asteroidX = x + vx , asteroidY = y + vy, asteroidXVector = vx, asteroidYVector = vy }
-
+-- this function changes the x and the y position of the bullet dependent of the xvector and the yvector of the bullet.
 
 changeBullet :: Bullet -> Bullet
 changeBullet Bullet {bulletX = x , bulletY = y, bulletXVector = vx, bulletYVector = vy} = Bullet { bulletX = x + vx , bulletY = y + vy,bulletXVector = vx, bulletYVector = vy }
-
+-- this function changes the x and the y position of the asteroid dependent of the xvector and the yvector of the asteroid.
 
 oobBullet :: Bullet -> Bool
 oobBullet Bullet {bulletX = x , bulletY = y} = ( x < -700) || ( y > 500 ) || ( x > 700 ) || ( y < -500 ) 
+-- this function checks if a bullet is out of bound.
 
 oobAsteroid :: Asteroid -> Asteroid
 oobAsteroid i@Asteroid{asteroidX = x , asteroidY = y, asteroidXVector = vx, asteroidYVector = vy} 
@@ -31,61 +32,91 @@ oobAsteroid i@Asteroid{asteroidX = x , asteroidY = y, asteroidXVector = vx, aste
   | x > 700 =  Asteroid { asteroidX = -700, asteroidY = y , asteroidXVector = vx, asteroidYVector = vy }
   | y < -500 = Asteroid { asteroidY = 500, asteroidX = x, asteroidXVector = vx, asteroidYVector = vy }
   | otherwise = id i
+{- this function checks if a asteroid is out of bound, if it is then the asteroid will be drawed at the other side of the screen opposite from where it left the screen.
+if the asteroid is not out of bound his position will not change.-}
 
+bulletAsteroidCollison :: [Bullet] -> [Asteroid] -> Bool
+bulletAsteroidCollison [] _ = False
+bulletAsteroidCollison _ [] = False
+bulletAsteroidCollison (bullet@Bullet { bulletX = x1, bulletY = y1 } :bullets) (asteroid@Asteroid { asteroidX = x2, asteroidY = y2}:asteroids) 
+  | (abs(x1-x2) <= 80) && (abs(y1-y2) <= 80)  = True 
+  | otherwise = bulletAsteroidCollison bullets asteroids  
+-- this function checks if a bullet is in collison with a asteroid.
+  
+playerAsteroidCollison :: Float -> Float -> [Asteroid] -> Bool
+playerAsteroidCollison _ _ []  = False
+playerAsteroidCollison x1 y1 (asteroid@Asteroid { asteroidX = x2, asteroidY = y2} :asteroids)
+ | (abs(x1-x2) <= 65) && (abs(y1-y2) <= 65) = True
+ | otherwise = playerAsteroidCollison x1 y1 asteroids
+-- this function checks if the player is in collison with a asteroid.   
+  
 outOfBoundY :: Float -> Float
 outOfBoundY x 
   | x < -500 = 500
   | x > 500 = -500
   | otherwise = x
+-- this function will gives the x position of a player is out of bound. if it is, then it will give the opposite x value back. Else the x position will not change.
  
 outOfBoundX :: Float -> Float
 outOfBoundX x
   | x < -500 = 500
   | x > 500 = -500
   | otherwise = x
-
-
+-- this function will gives the y position of a player is out of bound. if it is, then it will give the opposite y value back. Else the y position will not change.
+  
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
 step secs gstate = case gstate of
   GameMenu _ _ _ False -> do readscore <- readFile "HighScore.txt"
                              return $ gstate { highScoreList = readscore, readHighList = True}
+-- in the gamemenu state the game will read the highscore textfile and give it to the state and set the readhighlist boolean to True.
   GamePaused _ _ _ _ _ _ _ _ _ _ _ _ _ _ _-> return $ gstate { elapsedTime = elapsedTime gstate } 
-  GamePlaying _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _-> return $ gstate {elapsedTime = elapsedTime gstate + secs 
-                                                   ,bulletList =  filter (\x -> not (oobBullet x) )(map changeBullet (bulletList gstate))
+-- if the game is paused the time will stop.
+  GamePlaying _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _-> case lives gstate of 
+                                                   0 -> return (GameOver ShowGameOver 0 (score gstate) (highScoreList gstate) False)
+                                                   _ -> return $ gstate {elapsedTime = elapsedTime gstate + secs
+-- if the player has 0 lives left in the game, then the game is over and he will go to the gameover state, else game will be updated.
+                                                   ,bulletList =  filter (\x -> not (oobBullet x))(map changeBullet (bulletList gstate))												  
+-- the list of bullet will be updated dependent whether a bullet is out of bound, then the bullet will be deleted. and whetehr a bullet is shooted by the player.												   
                                                    ,asteroidList = map (oobAsteroid)(map changeAsteroid (asteroidList gstate))
-                                                   
+-- the list of asteroid will be updated dependent whether the asteroid is out of bound or not.												   
+												   ,score = if bulletAsteroidCollison (bulletList gstate) (asteroidList gstate) then score gstate + 25 else score gstate
+-- the score will be updated when a bullet hits a asteroid, then the score will add 25 points. Else the score will be the same.												   
+												   ,lives = if playerAsteroidCollison (xVector gstate) (yVector gstate) (asteroidList gstate) then lives gstate - 1 else lives gstate
+-- the lives will be updated when the player hits a asteroid, then the player will lose one live. Else the lives will be the same.
                                                    ,rotatespeed = case aPressed gstate || dPressed gstate of
                                                                   True -> 10
                                                                   _ -> 0
-
+-- the rotatespeed will be 10 when the player press the a or the d key. Else it will be zero.
                                                    ,leftVector =  case aPressed gstate of
                                                                   True  -> leftVector gstate - (rotatespeed gstate)
                                                                   _  -> leftVector gstate
-
+-- the player will rotate to the left when the a key is pressed.
                                                    ,rightVector =  case dPressed gstate of
                                                                    True -> rightVector gstate + (rotatespeed gstate)
                                                                    _ ->  rightVector gstate
-                                                   
-                                                   
+-- the player will rotate to the right when the d key is pressed.                                                                
                                                    ,movespeed =  case wPressed gstate of
                                                                  True ->  10
                                                                  _ -> 0
-
-                                                  , yVector = outOfBoundY (yVector gstate + (movespeed gstate * sinRotate))
-                                                  , xVector = outOfBoundX (xVector gstate  - (movespeed gstate * cosRotate))}
+-- the movespeed will be 10 when the player press the w key. Else it will be zero.
+                                                  , yVector = if playerAsteroidCollison (xVector gstate) (yVector gstate) (asteroidList gstate) then 0 else outOfBoundY (yVector gstate + (movespeed gstate * sinRotate))
+-- the yvector will be zero when the player collides with a asteroid. Else the player y postion will be updated and checked if it is out of bound.	  
+                                                  , xVector = if playerAsteroidCollison (xVector gstate) (yVector gstate) (asteroidList gstate) then 0 else outOfBoundX (xVector gstate  - (movespeed gstate * cosRotate))}
                                                       where sinRotate = sin(degreesToRad (90 + rightVector gstate + leftVector gstate ))
                                                             cosRotate = cos(degreesToRad (90 + rightVector gstate  + leftVector gstate )) 
-                                                                                                       
-                                                   
+-- the xvector will be zero when the player collides with a asteroid. Else the player x postion will be updated and checked if it is out of bound.                                                                                                                                                          
   GameOver _ _ _ _ False -> do 
                             let addscore = highScoreList gstate ++ " " ++ show(score gstate)
                             writescore <- writeFile ("HighScores.txt") addscore
                             copyscore <- writeFile "Highscore.txt" addscore
-                            return $ gstate { saved = True } 
+                            return $ gstate { saved = True }
+-- when the game is over the score that the player has reached, will be writen to a text file. 
   GameHighScore _ _ _ False -> do highScoreL <- readFile "HighScores.txt"
                                   return $ gstate { readed = True, highScoreList = highScoreL } 
+-- when the player is in the highscore gamestate, the highscore will be read from the textfile.
   _ -> return $ gstate { elapsedTime = elapsedTime gstate + secs }
+-- in every other sitiation the gamestate will be updated normally.
 --the step function handles the iterations of the game. it only updates the time when the game is not paused.
   {-| elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES 
   = -- We show a new random number
@@ -102,12 +133,11 @@ input e gstate = return (inputKey e gstate)
 
 createBullet :: Float -> Float -> Float -> Float -> Bullet
 createBullet px py vx vy =  Bullet { bulletX = px , bulletY = py ,  bulletXVector = vx, bulletYVector = vy }
-
+-- this function creates a bullet dependent on the position of the player.
 
 createAsteroid :: Float -> Float -> Float -> Float -> Asteroid
 createAsteroid px py vx vy =  Asteroid { asteroidX = px , asteroidY = py ,  asteroidXVector = vx, asteroidYVector = vy }
-
-
+-- this function creates a asteroid.
 
 randomAsteroid :: Int -> Int -> Int -> Int -> Asteroid
 randomAsteroid rx ry rvx rvy= createAsteroid (fst (randomR (-600,600) (mkStdGen rx))) (fst (randomR (-400,400) (mkStdGen ry))) (fst (randomR (-10,10) (mkStdGen rvx))) (fst (randomR (-10,10) (mkStdGen rvy))) 
@@ -118,9 +148,6 @@ degreesToRad :: Float -> Float
 degreesToRad x = x*(pi/180)
 -- the degreesToRad function convert a degree to a radian.
 
-
-
-
 inputKeyGame :: Event -> GameState -> GameState
 -- the inputKeyGame function handles all the player input when he is playing the game.
 inputKeyGame (EventKey (Char 'w') Down _ _) gstate
@@ -129,9 +156,6 @@ inputKeyGame (EventKey (Char 'w') Down _ _) gstate
 inputKeyGame (EventKey (Char 'w') Up _ _) gstate
  = gstate { wPressed = False }
  
-
-
-
 -- when the player press the w key the spaceship moves forward in the right direction.
 inputKeyGame (EventKey (Char 'a') Down _ _) gstate
  = gstate { aPressed = True }
@@ -159,10 +183,8 @@ inputKeyGame (EventKey (SpecialKey KeySpace ) Down _ _) gstate
   = gstate { bulletList = (bulletList gstate) ++ [createBullet (xVector gstate - (50*cosRotate)) (yVector gstate + (50*sinRotate))  (-15* cosRotate) ( 15*sinRotate)]}
               where sinRotate = sin(degreesToRad (90 + rightVector gstate + leftVector gstate ))
                     cosRotate = cos(degreesToRad (90 + rightVector gstate  + leftVector gstate ))
-             
-
-
-
+-- when the player press space, the player will shoot bullets.             
+			 
 inputKeyGame _ gstate = case lives gstate of
   0 -> GameOver ShowGameOver 0 (score gstate) (highScoreList gstate) False 
   _ -> gstate
